d523160d0399299603ddc7c0daca043733c0ec7a
Sean paul Shamia <seanzuri12@gmail.com>
2026-06-01 20:02:45 +0300

    added apollo.io for tracking and leads

--- next.config.ts
+++ next.config.ts
@@ -17,11 +17,11 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline'",
+      "script-src 'self' 'unsafe-inline' https://assets.apollo.io",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: blob:",
-      "connect-src 'self'",
+      "connect-src 'self' https://*.apollo.io",
       "frame-src 'none'",
       "object-src 'none'",
       "base-uri 'self'",
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk, JetBrains_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
@@ -79,6 +80,13 @@ export default function RootLayout({
         <main>{children}</main>
         <Footer />
       </body>
+      <Script
+        id="apollo-tracker"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,o.onload=function(){window.trackingFunctions.onLoad({appId:"6a06d2f8a53cf7000df498a1"})},document.head.appendChild(o)}initApollo();`,
+        }}
+      />
     </html>
   );
 }
